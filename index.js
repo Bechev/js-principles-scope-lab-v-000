53e34f2e96e14e095e3b523a7e0bf9ae44b83672
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-14 16:25:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-function upperCaseCustomerName(customer){
-  return customer.toUpperCase()
+function upperCaseCustomerName(){
+  return customerName.toUpperCase()
 }
